7f32056f9a89f35a425a04323231d494b7b0c446
sudoC7 <slowjacob7@gmail.com>
2024-03-07 11:42:06 +0100

    add from html

--- index 1.php
+++ index 1.php
@@ -1,5 +1,22 @@
-<?php
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOTEL</title>
+</head>
+<body>
     
+    <?php
 
-?>
+        spl_autoload_register(function($class_name){ 
+            require $class_name . '.php';
+        });
+
+        
+
+
+    ?>
+
+</body>
+</html>
